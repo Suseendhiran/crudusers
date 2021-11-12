ff1858da2c97e5afbd5522208040df79f542eb53
Suseendhiran D <suseendhiran.d@f22labs.com>
2021-11-12 13:02:58 +0530

    text changes

--- src/Components/AddEditCelebs/index.js
+++ src/Components/AddEditCelebs/index.js
@@ -23,80 +23,83 @@ function Index({ celebs, setCelebs }) {
   };
 
   return (
-    <Formik
-      initialValues={{
-        name: celebs[id]?.name ? celebs[id]?.name : "",
-        imageUrl: celebs[id]?.imageUrl ? celebs[id]?.imageUrl : "",
-        about: celebs[id]?.about ? celebs[id]?.about : "",
-        wikiUrl: celebs[id]?.wikiUrl ? celebs[id]?.wikiUrl : "",
-      }}
-      enableReinitialize
-      onSubmit={(values) => {
-        handleCelebDetails(values);
-      }}
-    >
-      {({ handleSubmit, values, handleChange }) => {
-        return (
-          <Box
-            component="form"
-            sx={{
-              "& > :not(style)": { m: 1, width: "80%" },
-            }}
-            onSubmit={handleSubmit}
-            autoComplete="off"
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-              margin: "auto",
-              justifyContent: "center",
-              alignItems: "flex-start",
-              marginTop: "30px",
-            }}
-          >
-            <TextField
-              id="outlined-basic"
-              required
-              label="Celebrity name"
-              variant="outlined"
-              value={values.name}
-              name="name"
-              onChange={handleChange}
-            />
-            <TextField
-              id="outlined-basic"
-              required
-              label="Image Url"
-              variant="outlined"
-              value={values.imageUrl}
-              name="imageUrl"
-              onChange={handleChange}
-            />
-            <TextField
-              id="outlined-basic"
-              required
-              label="About the Celeb"
-              variant="outlined"
-              value={values.about}
-              name="about"
-              onChange={handleChange}
-            />
-            <TextField
-              id="outlined-basic"
-              required
-              label="Wikipedia Url"
-              variant="outlined"
-              value={values.wikiUrl}
-              name="wikiUrl"
-              onChange={handleChange}
-            />
-            <Button variant="outlined" type="submit">
-              {id ? "Edit" : "Add"} Celebrity
-            </Button>
-          </Box>
-        );
-      }}
-    </Formik>
+    <>
+      <h1 className="edit-head">{id ? "Edit" : "Add"} Celebrity</h1>
+      <Formik
+        initialValues={{
+          name: celebs[id]?.name ? celebs[id]?.name : "",
+          imageUrl: celebs[id]?.imageUrl ? celebs[id]?.imageUrl : "",
+          about: celebs[id]?.about ? celebs[id]?.about : "",
+          wikiUrl: celebs[id]?.wikiUrl ? celebs[id]?.wikiUrl : "",
+        }}
+        enableReinitialize
+        onSubmit={(values) => {
+          handleCelebDetails(values);
+        }}
+      >
+        {({ handleSubmit, values, handleChange }) => {
+          return (
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": { m: 1, width: "80%" },
+              }}
+              onSubmit={handleSubmit}
+              autoComplete="off"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+                margin: "auto",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                marginTop: "30px",
+              }}
+            >
+              <TextField
+                id="outlined-basic"
+                required
+                label="Celebrity name"
+                variant="outlined"
+                value={values.name}
+                name="name"
+                onChange={handleChange}
+              />
+              <TextField
+                id="outlined-basic"
+                required
+                label="Image Url"
+                variant="outlined"
+                value={values.imageUrl}
+                name="imageUrl"
+                onChange={handleChange}
+              />
+              <TextField
+                id="outlined-basic"
+                required
+                label="About the Celeb"
+                variant="outlined"
+                value={values.about}
+                name="about"
+                onChange={handleChange}
+              />
+              <TextField
+                id="outlined-basic"
+                required
+                label="Wikipedia Url"
+                variant="outlined"
+                value={values.wikiUrl}
+                name="wikiUrl"
+                onChange={handleChange}
+              />
+              <Button variant="outlined" type="submit">
+                {id ? "Edit" : "Add"} Celebrity
+              </Button>
+            </Box>
+          );
+        }}
+      </Formik>
+    </>
   );
 }
 
