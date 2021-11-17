b935a609ed6fb891b1a1dca0e77d6291d6757e5b
Suseendhiran D <suseendhiran.d@f22labs.com>
2021-11-17 19:47:14 +0530

    remove celebs list form router

--- src/Providers/CelebritiesProvider.js
+++ src/Providers/CelebritiesProvider.js
@@ -5,6 +5,7 @@ export const celebrityContext = createContext();
 function CelebritiesProvider({ children }) {
   const [celebs, setCelebs] = useState([
     {
+      id: "100",
       name: "Rajinikanth",
       imageUrl:
         "https://images.news18.com/ibnlive/uploads/2021/07/1625107981_rajinikanth.jpg",
@@ -13,6 +14,7 @@ function CelebritiesProvider({ children }) {
       wikiUrl: "https://en.wikipedia.org/wiki/Rajinikanth",
     },
     {
+      id: "101",
       name: "Robert Downey Jr.",
       imageUrl:
         "https://www.cheatsheet.com/wp-content/uploads/2020/11/Marvel-star-Robert-Downey-Jr.jpg",
@@ -21,6 +23,7 @@ function CelebritiesProvider({ children }) {
       wikiUrl: "https://en.wikipedia.org/wiki/Robert_Downey_Jr.",
     },
     {
+      id: "102",
       name: "Tom Holland",
       imageUrl:
         "https://www.gannett-cdn.com/-mm-/cc053686530ce446f0a27dc352961fac33dd12ac/c=1144-81-2630-920/local/-/media/2017/06/26/USATODAY/USATODAY/636340759929048028-XXX-SPIDER-MAN-HOMECOMING-87249008.JPG",
@@ -29,6 +32,7 @@ function CelebritiesProvider({ children }) {
       wikiUrl: "https://en.wikipedia.org/wiki/Tom_Holland",
     },
     {
+      id: "103",
       name: "Robert Downey Jr.",
       imageUrl:
         "https://www.cheatsheet.com/wp-content/uploads/2020/11/Marvel-star-Robert-Downey-Jr.jpg",
@@ -37,6 +41,7 @@ function CelebritiesProvider({ children }) {
       wikiUrl: "https://en.wikipedia.org/wiki/Robert_Downey_Jr.",
     },
     {
+      id: "104",
       name: "Rajinikanth",
       imageUrl:
         "https://images.news18.com/ibnlive/uploads/2021/07/1625107981_rajinikanth.jpg",
@@ -45,6 +50,7 @@ function CelebritiesProvider({ children }) {
       wikiUrl: "https://en.wikipedia.org/wiki/Rajinikanth",
     },
     {
+      id: "105",
       name: "Robert Downey Jr.",
       imageUrl:
         "https://www.cheatsheet.com/wp-content/uploads/2020/11/Marvel-star-Robert-Downey-Jr.jpg",
@@ -53,6 +59,7 @@ function CelebritiesProvider({ children }) {
       wikiUrl: "https://en.wikipedia.org/wiki/Robert_Downey_Jr.",
     },
     {
+      id: "106",
       name: "Tom Holland",
       imageUrl:
         "https://www.gannett-cdn.com/-mm-/cc053686530ce446f0a27dc352961fac33dd12ac/c=1144-81-2630-920/local/-/media/2017/06/26/USATODAY/USATODAY/636340759929048028-XXX-SPIDER-MAN-HOMECOMING-87249008.JPG",
@@ -61,6 +68,7 @@ function CelebritiesProvider({ children }) {
       wikiUrl: "https://en.wikipedia.org/wiki/Tom_Holland",
     },
     {
+      id: "107",
       name: "Rajinikanth",
       imageUrl:
         "https://images.news18.com/ibnlive/uploads/2021/07/1625107981_rajinikanth.jpg",
@@ -69,6 +77,7 @@ function CelebritiesProvider({ children }) {
       wikiUrl: "https://en.wikipedia.org/wiki/Rajinikanth",
     },
     {
+      id: "108",
       name: "Robert Downey Jr.",
       imageUrl:
         "https://www.cheatsheet.com/wp-content/uploads/2020/11/Marvel-star-Robert-Downey-Jr.jpg",
@@ -77,6 +86,7 @@ function CelebritiesProvider({ children }) {
       wikiUrl: "https://en.wikipedia.org/wiki/Robert_Downey_Jr.",
     },
     {
+      id: "109",
       name: "Tom Holland",
       imageUrl:
         "https://www.gannett-cdn.com/-mm-/cc053686530ce446f0a27dc352961fac33dd12ac/c=1144-81-2630-920/local/-/media/2017/06/26/USATODAY/USATODAY/636340759929048028-XXX-SPIDER-MAN-HOMECOMING-87249008.JPG",
--- src/Router.js
+++ src/Router.js
@@ -4,99 +4,16 @@ import UsersList from "./Components/UsersList";
 import AddCelebs from "./Components/AddEditCelebs";
 
 function Router() {
-  const [celebs, setCelebs] = useState([
-    {
-      name: "Rajinikanth",
-      imageUrl:
-        "https://images.news18.com/ibnlive/uploads/2021/07/1625107981_rajinikanth.jpg",
-      about:
-        "Rajinikanth was born as Shivaji Rao Gaekwad on 12 December 1950 in a Marathi family in Bangalore, Mysore State ",
-      wikiUrl: "https://en.wikipedia.org/wiki/Rajinikanth",
-    },
-    {
-      name: "Robert Downey Jr.",
-      imageUrl:
-        "https://www.cheatsheet.com/wp-content/uploads/2020/11/Marvel-star-Robert-Downey-Jr.jpg",
-      about:
-        "Robert John Downey Jr. (born April 4, 1965)[1] is an American actor and producer. His career has been characterized by critical and popular success in his youth, followed by a period of substance abuse and legal troubles, before a resurgence of commercial success later in his career",
-      wikiUrl: "https://en.wikipedia.org/wiki/Robert_Downey_Jr.",
-    },
-    {
-      name: "Tom Holland",
-      imageUrl:
-        "https://www.gannett-cdn.com/-mm-/cc053686530ce446f0a27dc352961fac33dd12ac/c=1144-81-2630-920/local/-/media/2017/06/26/USATODAY/USATODAY/636340759929048028-XXX-SPIDER-MAN-HOMECOMING-87249008.JPG",
-      about:
-        "Thomas Stanley Holland (born 1 June 1996)[1] is an English actor. A graduate of the BRIT School for Performing Arts and Technology in London, he began his acting career on London stage in the title role of Billy Elliot the Musical in the West End theatre from 2008 to 2010. ",
-      wikiUrl: "https://en.wikipedia.org/wiki/Tom_Holland",
-    },
-    {
-      name: "Robert Downey Jr.",
-      imageUrl:
-        "https://www.cheatsheet.com/wp-content/uploads/2020/11/Marvel-star-Robert-Downey-Jr.jpg",
-      about:
-        "Robert John Downey Jr. (born April 4, 1965)[1] is an American actor and producer. His career has been characterized by critical and popular success in his youth, followed by a period of substance abuse and legal troubles, before a resurgence of commercial success later in his career",
-      wikiUrl: "https://en.wikipedia.org/wiki/Robert_Downey_Jr.",
-    },
-    {
-      name: "Rajinikanth",
-      imageUrl:
-        "https://images.news18.com/ibnlive/uploads/2021/07/1625107981_rajinikanth.jpg",
-      about:
-        "Rajinikanth was born as Shivaji Rao Gaekwad on 12 December 1950 in a Marathi family in Bangalore, Mysore State ",
-      wikiUrl: "https://en.wikipedia.org/wiki/Rajinikanth",
-    },
-    {
-      name: "Robert Downey Jr.",
-      imageUrl:
-        "https://www.cheatsheet.com/wp-content/uploads/2020/11/Marvel-star-Robert-Downey-Jr.jpg",
-      about:
-        "Robert John Downey Jr. (born April 4, 1965)[1] is an American actor and producer. His career has been characterized by critical and popular success in his youth, followed by a period of substance abuse and legal troubles, before a resurgence of commercial success later in his career",
-      wikiUrl: "https://en.wikipedia.org/wiki/Robert_Downey_Jr.",
-    },
-    {
-      name: "Tom Holland",
-      imageUrl:
-        "https://www.gannett-cdn.com/-mm-/cc053686530ce446f0a27dc352961fac33dd12ac/c=1144-81-2630-920/local/-/media/2017/06/26/USATODAY/USATODAY/636340759929048028-XXX-SPIDER-MAN-HOMECOMING-87249008.JPG",
-      about:
-        "Thomas Stanley Holland (born 1 June 1996)[1] is an English actor. A graduate of the BRIT School for Performing Arts and Technology in London, he began his acting career on London stage in the title role of Billy Elliot the Musical in the West End theatre from 2008 to 2010. ",
-      wikiUrl: "https://en.wikipedia.org/wiki/Tom_Holland",
-    },
-    {
-      name: "Rajinikanth",
-      imageUrl:
-        "https://images.news18.com/ibnlive/uploads/2021/07/1625107981_rajinikanth.jpg",
-      about:
-        "Rajinikanth was born as Shivaji Rao Gaekwad on 12 December 1950 in a Marathi family in Bangalore, Mysore State ",
-      wikiUrl: "https://en.wikipedia.org/wiki/Rajinikanth",
-    },
-    {
-      name: "Robert Downey Jr.",
-      imageUrl:
-        "https://www.cheatsheet.com/wp-content/uploads/2020/11/Marvel-star-Robert-Downey-Jr.jpg",
-      about:
-        "Robert John Downey Jr. (born April 4, 1965)[1] is an American actor and producer. His career has been characterized by critical and popular success in his youth, followed by a period of substance abuse and legal troubles, before a resurgence of commercial success later in his career",
-      wikiUrl: "https://en.wikipedia.org/wiki/Robert_Downey_Jr.",
-    },
-    {
-      name: "Tom Holland",
-      imageUrl:
-        "https://www.gannett-cdn.com/-mm-/cc053686530ce446f0a27dc352961fac33dd12ac/c=1144-81-2630-920/local/-/media/2017/06/26/USATODAY/USATODAY/636340759929048028-XXX-SPIDER-MAN-HOMECOMING-87249008.JPG",
-      about:
-        "Thomas Stanley Holland (born 1 June 1996)[1] is an English actor. A graduate of the BRIT School for Performing Arts and Technology in London, he began his acting career on London stage in the title role of Billy Elliot the Musical in the West End theatre from 2008 to 2010. ",
-      wikiUrl: "https://en.wikipedia.org/wiki/Tom_Holland",
-    },
-  ]);
-
   return (
     <Switch>
       <Route exact path="/">
         <UsersList />
       </Route>
       <Route path="/add-celeb">
-        <AddCelebs celebs={celebs} setCelebs={setCelebs} />
+        <AddCelebs />
       </Route>
       <Route path="/edit-celeb/:id">
-        <AddCelebs celebs={celebs} setCelebs={setCelebs} />
+        <AddCelebs />
       </Route>
     </Switch>
   );
